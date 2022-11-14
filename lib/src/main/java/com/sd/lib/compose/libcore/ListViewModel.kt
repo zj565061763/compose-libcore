6fbc208e4362b1e33e5a7ab1aa85aa184ced2fff
565061763 <565061763@qq.com>
2022-11-14 16:36:38 +0800

    ListViewModel

---
+++ lib/src/main/java/com/sd/lib/compose/libcore/ListViewModel.kt
@@ -0,0 +1,44 @@
+package com.sd.lib.compose.libcore
+
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+abstract class ListViewModel<T, D> : BaseViewModel<T>() {
+    protected val listState = ListState<D>()
+
+    val listUiState = listState.uiState
+
+    /**
+     * 加载更多数据
+     */
+    fun loadMoreData() {
+        if (!isActiveState) return
+        if (listUiState.value.isLoadingMore) return
+
+        viewModelScope.launch {
+            if (!isActiveState) return@launch
+            if (listUiState.value.isLoadingMore) return@launch
+            try {
+                listState.setLoadingMore(true)
+                mutator.mutate {
+                    loadMoreDataImpl()
+                }
+            } finally {
+                listState.setLoadingMore(false)
+            }
+        }
+    }
+
+    /**
+     * 加载更多数据
+     */
+    protected abstract suspend fun loadMoreDataImpl()
+
+    init {
+        viewModelScope.launch {
+            isRefreshing.collect {
+                listState.setRefreshing(it)
+            }
+        }
+    }
+}
