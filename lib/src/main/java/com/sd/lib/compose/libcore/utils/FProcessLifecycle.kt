32f739a1cd24846b7c660530b83184d74c6de762
zj565061763 <565061763@qq.com>
2022-12-11 14:42:26 +0800

    lifecycle-process

---
+++ lib/src/main/java/com/sd/lib/compose/libcore/utils/FProcessLifecycle.kt
@@ -0,0 +1,16 @@
+package com.sd.lib.compose.libcore.utils
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ProcessLifecycleOwner
+
+object FProcessLifecycle {
+    val lifecycle get() = ProcessLifecycleOwner.get().lifecycle
+
+    /**
+     * 当前是否处于前台
+     */
+    @JvmStatic
+    fun isForeground(): Boolean {
+        return lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)
+    }
+}
