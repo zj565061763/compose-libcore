1a9a7c3bf9978593b7ba2d5298ef494e2dabca7d
zj565061763 <565061763@qq.com>
2023-03-21 14:34:02 +0800

    isActive

--- lib/src/main/java/com/sd/lib/compose/libcore/core/FViewModel.kt
+++ lib/src/main/java/com/sd/lib/compose/libcore/core/FViewModel.kt
@@ -13,12 +13,14 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import java.lang.ref.WeakReference
 
-abstract class FViewModel<I> : ViewModel() {
+abstract class FViewModel<I>(
+    isActive: Boolean = true,
+) : ViewModel() {
     @Volatile
     private var _isDestroyed = false
 
     @Volatile
-    private var _isActiveState = true
+    private var _isActiveState = isActive
         set(value) {
             if (_isDestroyed) return
             synchronized(this@FViewModel) {
