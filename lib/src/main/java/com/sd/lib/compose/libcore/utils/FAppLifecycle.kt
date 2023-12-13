a45e37b002f9177522991ce38c35342d6e4544e4
zjun <565061763@qq.com>
2023-12-13 12:58:19 +0800

    FAppLifecycle

--- lib/src/main/java/com/sd/lib/compose/libcore/utils/FAppLifecycle.kt
+++ lib/src/main/java/com/sd/lib/compose/libcore/utils/FAppLifecycle.kt
@@ -1,10 +1,23 @@
 package com.sd.lib.compose.libcore.utils
 
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.ProcessLifecycleOwner
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 object FAppLifecycle {
+
+    @JvmStatic
     val lifecycle get() = ProcessLifecycleOwner.get().lifecycle
+
+    private val _mutableForegroundFlow = MutableStateFlow(isForeground())
+
+    /**
+     * App是否出于前台
+     */
+    val isForegroundFlow: StateFlow<Boolean> = _mutableForegroundFlow.asStateFlow()
 
     /**
      * 当前是否处于前台
@@ -12,5 +25,12 @@ object FAppLifecycle {
     @JvmStatic
     fun isForeground(): Boolean {
         return lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)
+    }
+
+    init {
+        val observer = LifecycleEventObserver { source, event ->
+            _mutableForegroundFlow.value = isForeground()
+        }
+        lifecycle.addObserver(observer)
     }
 }
