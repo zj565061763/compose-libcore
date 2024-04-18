600791e840f794f45c346d11ee5c2df380faf9a0
zjun <565061763@qq.com>
2024-04-18 23:09:53 +0800

    AppLifecycle

--- lib/src/main/java/com/sd/lib/compose/libcore/AppLifecycle.kt
+++ lib/src/main/java/com/sd/lib/compose/libcore/AppLifecycle.kt
@@ -3,6 +3,8 @@ package com.sd.lib.compose.libcore
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.ProcessLifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -10,7 +12,8 @@ import kotlinx.coroutines.flow.asStateFlow
 internal object AppLifecycle {
     private val _startedFlow = MutableStateFlow(fAppIsStarted)
 
-    val startedFlow get() = _startedFlow.asStateFlow()
+    val startedFlow: StateFlow<Boolean>
+        get() = _startedFlow.asStateFlow()
 
     init {
         fAppLifecycle.addObserver(
@@ -21,7 +24,18 @@ internal object AppLifecycle {
     }
 }
 
-val fAppLifecycle: Lifecycle get() = ProcessLifecycleOwner.get().lifecycle
+/** App生命周期 */
+val fAppLifecycle: Lifecycle
+    get() = ProcessLifecycleOwner.get().lifecycle
 
-val fAppIsStarted: Boolean get() = fAppLifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)
-val fAppIsStartedFlow: StateFlow<Boolean> get() = AppLifecycle.startedFlow
+/** App生命周期绑定的[CoroutineScope] */
+val fAppLifecycleScope: CoroutineScope
+    get() = ProcessLifecycleOwner.get().lifecycleScope
+
+/** App生命周期是否至少处于[Lifecycle.State.STARTED]状态 */
+val fAppIsStarted: Boolean
+    get() = fAppLifecycle.currentState.isAtLeast(Lifecycle.State.STARTED)
+
+/** 监听App生命周期是否至少处于[Lifecycle.State.STARTED]状态 */
+val fAppIsStartedFlow: StateFlow<Boolean>
+    get() = AppLifecycle.startedFlow
