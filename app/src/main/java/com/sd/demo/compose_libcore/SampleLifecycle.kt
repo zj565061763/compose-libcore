62c2fce9b10a8db3138f7a5c3368e3afd8be0ce2
zjun <565061763@qq.com>
2024-04-18 23:49:47 +0800

    demo

--- app/src/main/java/com/sd/demo/compose_libcore/SampleLifecycle.kt
+++ app/src/main/java/com/sd/demo/compose_libcore/SampleLifecycle.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LifecycleEventEffect
+import androidx.lifecycle.lifecycleScope
 import com.sd.demo.compose_libcore.ui.theme.AppTheme
 import com.sd.lib.compose.libcore.fAppIsStartedFlow
+import kotlinx.coroutines.launch
 
 class SampleLifecycle : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +17,12 @@ class SampleLifecycle : ComponentActivity() {
         setContent {
             AppTheme {
                 Content()
+            }
+        }
+
+        lifecycleScope.launch {
+            fAppIsStartedFlow.collect {
+                logMsg { "App started:$it" }
             }
         }
     }
@@ -26,14 +33,7 @@ private fun Content() {
     LifecycleEventEffect(Lifecycle.Event.ON_START) {
         logMsg { "ON_START" }
     }
-
     LifecycleEventEffect(Lifecycle.Event.ON_STOP) {
         logMsg { "ON_STOP" }
-    }
-
-    LaunchedEffect(Unit) {
-        fAppIsStartedFlow.collect {
-            logMsg { "App started:$it" }
-        }
     }
 }
